ffcdda5a1490eb55b07df3de265ae8e55ba5dadc
vnewto <181774283+vnewto@users.noreply.github.com>
2025-09-12 20:35:34 -0400

    feat: add InfoWindow popup when a marker is clicked

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {
   useMap,
   AdvancedMarker,
   Marker,
+  InfoWindow,
 } from "@vis.gl/react-google-maps";
 import { useState, useRef, useEffect } from "react";
 import CreateCenterControl from "./features/map";
@@ -19,6 +20,7 @@ function App() {
   const testMarkers = [
     {
       name: "testMarkers1",
+      randomWord: "yelp",
       location: {
         lat: 42.38782221947883,
         lng: -71.23670789679711,
@@ -26,6 +28,7 @@ function App() {
     },
     {
       name: "testMarkers2",
+      randomWord: "snipe",
       location: {
         lat: 42.405697803295766,
         lng: -71.24311923980713,
@@ -33,12 +36,16 @@ function App() {
     },
     {
       name: "testMarkers3",
+      randomWord: "magenta",
       location: {
         lat: 42.36863938831473,
         lng: -71.25199387199015,
       },
     },
   ];
+
+  // set Usestate for whether a marker has been clicked on (to display info window)
+  const [selectedMarker, setSelectedMarker] = useState("");
 
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
@@ -69,12 +76,27 @@ function App() {
           {testMarkers.map((marker) => {
             return (
               <div key={marker.name}>
-                <AdvancedMarker position={marker.location}>
+                <AdvancedMarker
+                  position={marker.location}
+                  // add onClick event listener for when a user clicks on a marker
+                  onClick={() => {
+                    setSelectedMarker(marker);
+                  }}
+                >
                   <span style={{ fontSize: "2rem" }}>☀️</span>
                 </AdvancedMarker>
               </div>
             );
           })}
+          {/* if selectedMarker isn't empty, show the info window */}
+          {selectedMarker && (
+            <InfoWindow position={selectedMarker.location}>
+              <h1>{selectedMarker.name}</h1>
+              <p>{selectedMarker.randomWord}</p>
+
+              <button onClick={() => setSelectedMarker("")}>Close</button>
+            </InfoWindow>
+          )}
         </Map>
       </APIProvider>
     </div>
